a79b8ac27d0dece5c775a8cd6408bcf810a962c4
Liliana93 <liliana1000000@gmail.com>
2018-08-10 01:46:55 +0300

    commit 11.08.2018

--- src/test/java/steps/Stepdef.java
+++ src/test/java/steps/Stepdef.java
@@ -1,8 +1,8 @@
 package steps;
 
+import classes.RestAPIHandler;
 import context.Context;
 import context.ScenarioContext;
-import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -10,11 +10,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import classes.RestAPIHandler;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -22,47 +19,22 @@ import java.util.Properties;
 
 
 public class Stepdef {
-    public static RequestSpecification httpRequest;
     public Properties prop;
     public static Logger log = LogManager.getLogger(Stepdef.class.getName());
-    private ScenarioContext scenarioContext ;
-    public Stepdef (ScenarioContext scenarioContext){
+    private ScenarioContext scenarioContext;
+
+    public Stepdef(ScenarioContext scenarioContext) {
         this.scenarioContext = scenarioContext;
     }
+
     @Before()
     public void init() throws IOException {
         prop = new Properties();
         FileInputStream dataFile = new FileInputStream("C:\\copy3\\imgur.projectTest\\src\\main\\resources\\data.properties");
         prop.load(dataFile);
         RestAssured.baseURI = prop.getProperty("flickrbaseUrl");
-
-        /*public static WebDriver driver;
-
-    public WebDriver initializeDriver() throws IOException {
-
-        prop = new Properties();
-        FileInputStream dataFile = new FileInputStream("C:\\TestFramework\\src\\main\\resources\\data.properties");
-
-        prop.load(dataFile);
-        String browserName = prop.getProperty("browser");
-        System.out.println(browserName);
-
-        if (browserName.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "C:\\Soft\\Webdriver\\chromedriver.exe");
-            driver = new ChromeDriver();
-
-        } else if (browserName.equals("firefox")) {
-            System.setProperty("webdriver.gecko.driver",  "C:\\Soft\\Webdriver\\geckodriver.exe");
-            driver = new FirefoxDriver();
-        } else if (browserName.equals("IE")) {
-
-        }
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        return driver;
     }
-*/
-    }
+
     @Given("^user makes a Get Request$")
     public void userMakesAGetRequest() throws Throwable {
         log.info("Making a Get request");
@@ -72,10 +44,11 @@ public class Stepdef {
 
     @Then("^Response Code should be (\\d+)$")
     public void responseCodeShouldBe(int code) throws Throwable {
-        Response response = (Response)scenarioContext.getContext(Context.Response);
+        Response response = (Response) scenarioContext.getContext(Context.Response);
         int responseStatusCode = response.getStatusCode();
         Assert.assertEquals(responseStatusCode, code, "Incorrect status code returned");
         log.info("Response code is 200");
     }
+
 
 }
---
+++ src/test/java/steps/WebdriverSteps.java
@@ -0,0 +1,59 @@
+package steps;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import io.restassured.RestAssured;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WebdriverSteps {
+    public static WebDriver driver;
+    public Properties prop;
+    public static Logger log = LogManager.getLogger(Stepdef.class.getName());
+
+    @Before()
+    public WebDriver initializeDriver() throws IOException {
+        prop = new Properties();
+        FileInputStream dataFile = new FileInputStream("C:\\copy3\\imgur.projectTest\\src\\main\\resources\\data.properties");
+        prop.load(dataFile);
+        RestAssured.baseURI = prop.getProperty("flickrbaseUrl");
+        String browserName = prop.getProperty("browser");
+        System.out.println(browserName);
+
+        if (browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Soft\\Webdriver\\chromedriver.exe");
+            driver = new ChromeDriver();
+
+        } else if (browserName.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "C:\\Soft\\Webdriver\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else if (browserName.equals("IE")) {
+
+        }
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    @Given("^user accesses the YouTube Page$")
+    public void userAccessesTheYouTubePage() throws Throwable {
+        driver.get(prop.getProperty("url"));
+        log.info("User has accessed the YouTube");
+
+    }
+
+    @After
+    public void teardown() {
+        driver.close();
+        driver = null;
+    }
+}
